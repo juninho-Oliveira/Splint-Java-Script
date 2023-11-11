31866e38a3869f62efbcb17e0d1862c605fa0a7b
junior <junior.oliveira05@icloud.com>
2023-11-11 17:56:20 -0300

    Dom

---
+++ main.js
@@ -0,0 +1,13 @@
+//Função para troca fonte do paragrafo 
+
+function mudaTamanhoFonte() {
+
+    document.getElementById("manipula").style.fontSize = "38px";
+
+}
+
+//Função muda cor de texto do paragrafo 
+function mudaCorParagrafo() {
+
+    document.getElementById("manipula").style.backgroundColor = "black";
+}
